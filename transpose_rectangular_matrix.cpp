ffb099a4e502b7cf197dadda277241d31e3fdcdd
Melissa Dibé <94681600+thisismemeli@users.noreply.github.com>
2023-02-12 20:12:15 +0300

    Update transpose_rectangular_matrix.cpp

--- transpose_rectangular_matrix.cpp
+++ transpose_rectangular_matrix.cpp
@@ -1,3 +1,10 @@
+/*Given a two-dimensional array of size n × m (n, m <1000). The array symmetrical by the main diagonal is called transposed to original one.
+Array has dimensions m × n: the rows of the original array become the columns of the transposed one. 
+The columns of the original array become the rows of the transposed.
+For given two-dimensional array, build the transposed one.
+transpose function should have following prototype:
+std :: vector <std :: vector <int > > transpose (const std :: vector <std :: vector <int > > & matrix).*/
+
 #include <iostream>
 #include <vector>
 using namespace std;
