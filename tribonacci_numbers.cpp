485e80fee828c1f9d4f6834754e93fba241729c7
Melissa Dibé <94681600+thisismemeli@users.noreply.github.com>
2023-02-12 20:15:52 +0300

    Update tribonacci_numbers.cpp

--- tribonacci_numbers.cpp
+++ tribonacci_numbers.cpp
@@ -1,3 +1,15 @@
+/*
+Tribonacci numbers are a sequence of integers, which is defined as follows:
+the first, second and third numbers of Tribonacci are equal to one;
+Each following Tribonacci number is equal to the sum of the three previous ones.
+In general, almost like Fibonacci numbers.
+Write a program that calculates Tribonacci numbers.
+Input format
+One natural number N (N <= 75) is entered.
+Output format
+The first N Tribonacci numbers are displayed.
+*/
+
 #include <iostream>
 
 int main() 
