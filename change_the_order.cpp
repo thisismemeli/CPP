0614d9698f549f2203c5f6de33824be55b73cf48
Melissa Dibé <94681600+thisismemeli@users.noreply.github.com>
2023-02-12 20:11:27 +0300

    Update change_the_order.cpp

--- change_the_order.cpp
+++ change_the_order.cpp
@@ -1,3 +1,8 @@
+/* Write a reverse function that accepts a reference to std::vector<int> 
+and rearranges the array objects in reverse order.
+The function header should be like this:
+void reverse(std::vector<int>& numbers)*/
+
 #include <algorithm>
 #include <vector>
 
