359f4f3cf7066b3a40a066adc76f70024727982c
Melissa Dibé <94681600+thisismemeli@users.noreply.github.com>
2023-02-12 20:10:50 +0300

    Update extract_digits.cpp

--- extract_digits.cpp
+++ extract_digits.cpp
@@ -1,3 +1,7 @@
+/* The program takes a string as input. The input string may contain spaces. 
+Extract all numbers from this string and return the result as a new string.
+Submit solution as a function: std::string ExtractDigits(const std::string& s) */
+
 std::string ExtractDigits(const std::string &s) 
 {
     std::string answer = "";
