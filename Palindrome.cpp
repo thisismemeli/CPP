d6423b40decbdfb5f1b6cfa11205ad4c2401cdbf
Melissa Dibé <94681600+thisismemeli@users.noreply.github.com>
2023-02-12 20:07:37 +0300

    Update Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,3 +1,11 @@
+/*A string consisting of lowercase Latin letters and spaces is given. Check whether it is a palindrome without spaces (eg, "no lemon no melon").
+The solution has to include functional decomposition. Failure to follow the decomposition requirements will result in the lower score, even if the solution gets "OK" from the contest checker.
+Requirement: Implement the solution as a function that takes a string as input and returns true/false depending on whether the string is a palindrome or not. Use this function in the function `main()` to solve the problem.
+Input format
+The input is 1 line of not more than 100 characters, including spaces. There can be an arbitrary number of spaces between words.
+Output format
+Output "yes" if the given string is a palindrome, and "no" otherwise.*/
+
 #include <iostream>
 #include <string>
 
